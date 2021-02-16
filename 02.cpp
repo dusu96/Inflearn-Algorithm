c3df3b2e6c100a5cb6bc1f31fa66c2de1ba7c7de
dusu96 <71583910+dusu96@users.noreply.github.com>
2021-02-16 22:50:16 +0900

    [Inflearn] 자연수의 합
    코드구현력 기르기

---
+++ 02.cpp
@@ -0,0 +1,18 @@
+﻿#include<iostream>
+using namespace std;
+
+int main() {
+	int a, b, i, sum = 0;
+
+	cin >> a >> b;
+
+	for (i = a; i < b; i++) {
+		sum += i;
+		cout << i << " + ";
+	}
+
+	cout << b << " = "; // "=" 을 붙히기 위해 마지막 값을 따로 가져옴
+	cout << sum + b;
+
+	return 0;
+}
