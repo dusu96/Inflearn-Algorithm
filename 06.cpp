d32b6c65f3ecf1e0623286ec7d45635c4b8d470c
dusu96 <71583910+dusu96@users.noreply.github.com>
2021-02-17 23:16:43 +0900

    [Inflearn] 숫자만 추출
    코드구현력 기르기

---
+++ 06.cpp
@@ -0,0 +1,24 @@
+﻿#include<iostream>
+using namespace std;
+
+int main() {
+	char arr[50];
+	int i, a, num = 0, cnt = 0; // num: 자연수, cnt: 약수의 개수
+
+	cin >> arr;
+
+	for (i = 0; i < strlen(arr); i++) { // 문자열 배열 길이 구하는 함수 strlen(arr)
+		if (arr[i] >= 48 && arr[i] <= 57) {
+			num = num * 10 + (arr[i] - 48);
+		}
+	}
+	
+	for (i = 1; i <= num; i++) {
+		if (num % i == 0) {
+			cnt++;
+		}
+	}
+
+	cout << num << "\n" << cnt;
+	return 0;
+}
